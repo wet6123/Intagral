234b7254af2262ace3cfb30cf097b1a2e57f4e50
한유연 <hyy0416@naver.com>
2022-10-30 18:54:08 +0900

    [APP] feat | login 기능 구현중

--- intagralApp/app/src/main/java/com/ssafy/intagral/MainActivity.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.ssafy.intagral
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
-
+import com.google.android.gms.common.api.ApiException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,5 +47,41 @@ class MainActivity : AppCompatActivity() {
 
     fun updateUI(recentLoginAccount: GoogleSignInAccount?) {
         //update ui
+    }
+
+    fun onClick(v: View) {
+        when (v.getId()) {
+            R.id.sign_in_button -> signIn()
+        }
+    }
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
     }
 }
