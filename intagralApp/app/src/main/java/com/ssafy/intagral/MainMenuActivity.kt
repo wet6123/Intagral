1830283f0d5e675d5e3b4e821dd80ff3fed250a3
한유연 <hyy0416@naver.com>
2022-10-31 16:53:24 +0900

    [APP] feat | WIP home fragment

--- intagralApp/app/src/main/java/com/ssafy/intagral/MainMenuActivity.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/MainMenuActivity.kt
@@ -34,8 +34,9 @@ class MainMenuActivity : AppCompatActivity() {
                 logout()
                 finish()
             }
+            setHome()
         }
-        setHome()
+
     }
     private fun setHome() {
         println("setHome")
--- intagralApp/app/src/main/java/com/ssafy/intagral/data/source/PostItem.kt
+++
@@ -1,6 +0,0 @@
-package com.ssafy.intagral.data
-
-data class PostItem (
-    val postId: Int,
-    val imgPath: String
-)
