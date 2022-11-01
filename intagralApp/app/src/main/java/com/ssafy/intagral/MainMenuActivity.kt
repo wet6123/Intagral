0520cf456897e46182c87d0443dfd026b11d96c2
한유연 <hyy0416@naver.com>
2022-11-01 09:32:31 +0900

    [APP] merge | merge APP/homeFragment into develop

--- intagralApp/app/src/main/java/com/ssafy/intagral/MainMenuActivity.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/MainMenuActivity.kt
@@ -4,21 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.FragmentTransaction
+import androidx.transition.FragmentTransitionSupport
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.ssafy.intagral.databinding.ActivityMainMenuBinding
+import com.ssafy.intagral.ui.home.HomeFragment
 import com.ssafy.intagral.ui.upload.PhotoPicker
 
 class MainMenuActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainMenuBinding
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        mGoogleSignInClient = this.let { GoogleSignIn.getClient(it, gso) }
+
         binding = ActivityMainMenuBinding.inflate(layoutInflater).apply {
             setContentView(root)
             menuBottomNavigation.setOnItemSelectedListener(BottomTabListener())
+            tmpLogoutBtn.setOnClickListener{
+                logout()
+                finish()
+            }
+            setHome()
         }
+
+    }
+    private fun setHome() {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.menu_frame_layout, HomeFragment()).commit()
     }
 
     inner class BottomTabListener : NavigationBarView.OnItemSelectedListener{
@@ -28,6 +49,7 @@ class MainMenuActivity : AppCompatActivity() {
             when(item?.itemId){
                 R.id.nav_home -> {
                     println("home selected!")
+                    setHome()
                 }
                 R.id.nav_upload -> {
                     println("upload selected!")
@@ -46,5 +68,13 @@ class MainMenuActivity : AppCompatActivity() {
 
             return true
         }
+    }
+
+    private fun logout() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                // 로그아웃 성공시 실행
+                // 로그아웃 이후의 이벤트들(토스트 메세지, 화면 종료)을 여기서 수행하면 됨
+            }
     }
 }
