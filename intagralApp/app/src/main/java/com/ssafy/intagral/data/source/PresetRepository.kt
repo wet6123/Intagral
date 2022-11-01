7f5ee3b8895532c7a0f1c1b7d4a8beebfd5768d4
정태윤 <tanficial9574@gmail.com>
2022-11-01 15:24:03 +0900

    [APP] feat | resultTagFragment

--- intagralApp/app/src/main/java/com/ssafy/intagral/data/source/PresetRepository.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/data/source/PresetRepository.kt
@@ -1,2 +1,12 @@
 package com.ssafy.intagral.data.source
 
+import com.ssafy.intagral.data.Preset
+
+class PresetRepository {
+    private val presetSource : PresetSource = PresetSourceLocalImpl()
+
+    fun getPresetList() : Preset{
+        return presetSource.fetchPresetData()
+
+    }
+}
