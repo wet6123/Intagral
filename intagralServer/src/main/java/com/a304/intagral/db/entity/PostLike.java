96eee3a111e7fb9a24c540ab005a8f12a4192c03
강봉민 <kbm4250@naver.com>
2022-11-04 12:24:37 +0900

    [SERVER] merge | merge SERVER/jpa into develop

---
+++ intagralServer/src/main/java/com/a304/intagral/db/entity/PostLike.java
@@ -0,0 +1,30 @@
+package com.a304.intagral.db.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(
+        columnNames = {"user_id", "post_id"})
+)
+public class PostLike extends BaseEntity {
+
+    @Id
+    @Column(name = "post_like_id", columnDefinition = "INT UNSIGNED", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, insertable = false, updatable = false)
+    User postLikeToUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false, insertable = false, updatable = false)
+    Post postLikeToPost;
+}
