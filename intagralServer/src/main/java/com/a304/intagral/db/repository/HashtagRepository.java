650330bbef8593af4cabe5dc330bc49a5820593c
강봉민 <kbm4250@naver.com>
2022-11-06 04:27:09 +0900

    [SERVER] merge | merge SERVER/search into develop

--- intagralServer/src/main/java/com/a304/intagral/db/repository/HashtagRepository.java
+++ intagralServer/src/main/java/com/a304/intagral/db/repository/HashtagRepository.java
@@ -4,10 +4,13 @@ import com.a304.intagral.db.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
     Optional<Hashtag> findByContent(String content);
+
+    List<Hashtag> findAllByContentContaining(String target);
 }
