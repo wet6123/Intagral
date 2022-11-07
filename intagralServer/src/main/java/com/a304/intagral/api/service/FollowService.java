43cc0ff743e502fe48c910f433a2481afdb4f79b
강봉민 <kbm4250@naver.com>
2022-11-07 17:31:11 +0900

    [SERVER] feat | impl follow/list API

--- intagralServer/src/main/java/com/a304/intagral/api/service/FollowService.java
+++ intagralServer/src/main/java/com/a304/intagral/api/service/FollowService.java
@@ -1,11 +1,20 @@
 package com.a304.intagral.api.service;
 
-import com.a304.intagral.db.dto.FollowHashtagPostDto;
-import com.a304.intagral.db.dto.FollowUserPostDto;
+import com.a304.intagral.db.dto.*;
+
+import java.util.List;
 
 public interface FollowService {
 
     FollowUserPostDto toggleUserFollow(Long userId, String nickname);
 
     FollowHashtagPostDto toggleHashtagFollow(Long userId, String hashtag);
+
+    List<FollowUserGetDto> getUserFollowingList(Long userId, String nickname);
+
+    List<FollowUserGetDto> getFollowerList(Long userId, String nickname);
+
+    List<FollowingHashtagGetDto> getHashtagFollowingList(Long userId, String nickname);
+
+    List<FollowUserGetDto> getHashtagFollowerList(Long userId, String hashtag);
 }
--- intagralServer/src/main/java/com/a304/intagral/api/service/FollowServiceImpl.java
+++ intagralServer/src/main/java/com/a304/intagral/api/service/FollowServiceImpl.java
@@ -1,7 +1,6 @@
 package com.a304.intagral.api.service;
 
-import com.a304.intagral.db.dto.FollowHashtagPostDto;
-import com.a304.intagral.db.dto.FollowUserPostDto;
+import com.a304.intagral.db.dto.*;
 import com.a304.intagral.db.entity.Hashtag;
 import com.a304.intagral.db.entity.HashtagFollow;
 import com.a304.intagral.db.entity.User;
@@ -12,6 +11,9 @@ import com.a304.intagral.db.repository.UserFollowRepository;
 import com.a304.intagral.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("followService")
 public class FollowServiceImpl implements FollowService {
@@ -76,5 +78,116 @@ public class FollowServiceImpl implements FollowService {
                 .build();
 
         return followHashtagPostDto;
+    }
+
+    // TODO: N + 1
+    @Override
+    public List<FollowUserGetDto> getUserFollowingList(Long userId, String nickname) {
+        User targetUser = userRepository.findByNickname(nickname).get();
+
+        List<UserFollow> userFollowingList = targetUser.getUserFollowingList();
+        List<Long> userIdList = new ArrayList<>();
+        for (UserFollow userFollow : userFollowingList) {
+            userIdList.add(userFollow.getUserIdTo().longValue());
+        }
+        List<User> userList = userRepository.findByIdIn(userIdList);
+
+        List<FollowUserGetDto> followList = new ArrayList<>();
+        for (User user : userList) {
+            boolean isFollow = userFollowRepository.countByUserIdToAndUserIdFrom(userId.intValue(), user.getId().intValue()) != 0;
+            FollowUserGetDto followUserGetDto = FollowUserGetDto.builder()
+                    .imagePath(user.getProfileImgPath())
+                    .nickname(user.getNickname())
+                    .intro(user.getIntro())
+                    .followerCnt(user.getUserFollowerList().size())
+                    .isFollow(isFollow)
+                    .build();
+
+            followList.add(followUserGetDto);
+        }
+
+        return followList;
+    }
+
+    @Override
+    public List<FollowUserGetDto> getFollowerList(Long userId, String nickname) {
+        User targetUser = userRepository.findByNickname(nickname).get();
+
+        List<UserFollow> userFollowerList = targetUser.getUserFollowerList();
+        List<Long> userIdList = new ArrayList<>();
+        for (UserFollow userFollow : userFollowerList) {
+            userIdList.add(userFollow.getUserIdFrom().longValue());
+        }
+        List<User> userList = userRepository.findByIdIn(userIdList);
+
+        List<FollowUserGetDto> followList = new ArrayList<>();
+        for (User user : userList) {
+            boolean isFollow = userFollowRepository.countByUserIdToAndUserIdFrom(userId.intValue(), user.getId().intValue()) != 0;
+            FollowUserGetDto followUserGetDto = FollowUserGetDto.builder()
+                    .imagePath(user.getProfileImgPath())
+                    .nickname(user.getNickname())
+                    .intro(user.getIntro())
+                    .followerCnt(user.getUserFollowerList().size())
+                    .isFollow(isFollow)
+                    .build();
+
+            followList.add(followUserGetDto);
+        }
+
+        return followList;
+    }
+
+    @Override
+    public List<FollowingHashtagGetDto> getHashtagFollowingList(Long userId, String nickname) {
+        User targetUser = userRepository.findByNickname(nickname).get();
+
+        List<HashtagFollow> hashtagFollowList = targetUser.getHashtagFollowList();
+        List<Long> hashtagIdList = new ArrayList<>();
+        for (HashtagFollow hashtagFollow : hashtagFollowList) {
+            hashtagIdList.add(hashtagFollow.getHashtagId().longValue());
+        }
+        List<Hashtag> hashtagList = hashtagRepository.findByIdIn(hashtagIdList);
+
+        List<FollowingHashtagGetDto> followList = new ArrayList<>();
+        for (Hashtag hashtag : hashtagList) {
+            boolean isFollow = hashtagFollowRepository.countByUserIdAndHashtagId(userId.intValue(), hashtag.getId().intValue()) != 0;
+            FollowingHashtagGetDto followingHashtagGetDto = FollowingHashtagGetDto.builder()
+                    .content(hashtag.getContent())
+                    .followerCnt(hashtag.getHashtagFollowList().size())
+                    .isFollow(isFollow)
+                    .build();
+
+            followList.add(followingHashtagGetDto);
+        }
+
+        return followList;
+    }
+
+    @Override
+    public List<FollowUserGetDto> getHashtagFollowerList(Long userId, String hashtag) {
+        Hashtag targetHashtag = hashtagRepository.findByContent(hashtag).get();
+
+        List<HashtagFollow> hashtagFollowList = targetHashtag.getHashtagFollowList();
+        List<Long> userIdList = new ArrayList<>();
+        for (HashtagFollow hashtagFollow : hashtagFollowList) {
+            userIdList.add(hashtagFollow.getUserId().longValue());
+        }
+        List<User> userList = userRepository.findByIdIn(userIdList);
+
+        List<FollowUserGetDto> followList = new ArrayList<>();
+        for (User user : userList) {
+            boolean isFollow = userFollowRepository.countByUserIdToAndUserIdFrom(userId.intValue(), user.getId().intValue()) != 0;
+            FollowUserGetDto followUserGetDto = FollowUserGetDto.builder()
+                    .imagePath(user.getProfileImgPath())
+                    .nickname(user.getNickname())
+                    .intro(user.getIntro())
+                    .followerCnt(user.getUserFollowerList().size())
+                    .isFollow(isFollow)
+                    .build();
+
+            followList.add(followUserGetDto);
+        }
+
+        return followList;
     }
 }
