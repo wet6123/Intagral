12d6a1ed15f0efce74b195aaf61a005fbac7ccac
한유연 <hyy0416@naver.com>
2022-11-08 09:55:41 +0900

    [APP] feat | add Auth interceptor

--- intagralApp/app/src/main/java/com/ssafy/intagral/di/IntagralModule.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/di/IntagralModule.kt
@@ -1,6 +1,7 @@
 package com.ssafy.intagral.di
 
 import com.ssafy.intagral.data.source.preset.PresetRepository
+import com.ssafy.intagral.util.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,20 +17,10 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object CommonService {
     private val BASE_URL = "https://k7a304.p.ssafy.io"
-    private var token: String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2IiwiaXNzIjoiaW50YWdyYWwuY29tIiwiZXhwIjoxNjY4OTI2MTMwLCJpYXQiOjE2Njc2MzAxMzB9.85xqJtZn5uXyNhX11yqCsR6mXwhEyUWPWZA9boBKStqUu2moFbFYBMLjPJUE4L9y5z7IyLKee1KQxs3Gi-gwIg"
 
     private val client: OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor())
-        .addInterceptor {
-            // Request
-            val request = it.request()
-                .newBuilder()
-                .addHeader("Authorization", "Bearer $token")
-                .build()
-            // Response
-            val response = it.proceed(request)
-            response
-        }
+        .addInterceptor(AuthInterceptor())
         .build()
 
     @Singleton
