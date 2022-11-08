a638f09bc776779525174c890aa3d03a12095124
JunSeong <gentlmanjun@naver.com>
2022-11-09 04:02:46 +0900

    [SERVER] feat | Impl like post api

--- intagralServer/src/main/java/com/a304/intagral/api/response/PostLikePostRes.java
+++ intagralServer/src/main/java/com/a304/intagral/api/response/PostLikePostRes.java
@@ -1,11 +1,26 @@
 package com.a304.intagral.api.response;
 
+import com.a304.intagral.common.response.BaseResponseBody;
+import com.a304.intagral.dto.PostLikePostDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class PostLikePostRes {
+public class PostLikePostRes extends BaseResponseBody {
+    @JsonProperty(value = "isLike")
     boolean isLike;
+    @JsonProperty(value = "likeCnt")
     Integer likeCnt;
+
+    static public PostLikePostRes of(int statusCode, String message, PostLikePostDto postLikePostDto) {
+        PostLikePostRes res = new PostLikePostRes();
+        res.setStatusCode(statusCode);
+        res.setMessage(message);
+        res.setLike(postLikePostDto.isLike());
+        res.setLikeCnt(postLikePostDto.getLikeCnt());
+
+        return res;
+    }
 }
