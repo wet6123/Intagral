ef85c4113a56727740f7087bf864445a7f3767bd
강봉민 <kbm4250@naver.com>
2022-11-09 13:28:13 +0900

    [SERVER] merge | SERVER/swagger into develop

--- intagralServer/src/main/java/com/a304/intagral/api/response/PresetListRes.java
+++ intagralServer/src/main/java/com/a304/intagral/api/response/PresetListRes.java
@@ -2,6 +2,7 @@ package com.a304.intagral.api.response;
 
 import com.a304.intagral.common.response.BaseResponseBody;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +10,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Schema(description = "프리셋 리스트 Response")
 @Getter
 @Setter
 public class PresetListRes extends BaseResponseBody {
-
+    @Schema(name = "대상 클래스 리스트", example = "default, chair, desk")
     @JsonProperty(value = "class")
     List<String> clsList;
+    @Schema(name = "프리셋 리스트", example = "허먼밀러, 4Legs")
     @JsonProperty(value = "data")
     Map<String, List<String>> presetList = new HashMap<>();
 
--- intagralServer/src/main/java/com/a304/intagral/api/response/UserLoginPostRes.java
+++ intagralServer/src/main/java/com/a304/intagral/api/response/UserLoginPostRes.java
@@ -1,13 +1,15 @@
 package com.a304.intagral.api.response;
 
 import com.a304.intagral.common.response.BaseResponseBody;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "auth Token Response")
 @Getter
 @Setter
 public class UserLoginPostRes extends BaseResponseBody {
-
+    @Schema(name = "Auth Token", example = "E9DA4546CASADW21cxz...")
     String authToken;
 
     public static UserLoginPostRes of(Integer statusCode, String message, String accessToken) {
