55390d360e786371cca35e94f3f36eb1dff9e220
정태윤 <tanficial9574@gmail.com>
2022-11-10 16:16:15 +0900

    [APP] feat | post detail fragment, follow, like

---
+++ intagralApp/app/src/main/java/com/ssafy/intagral/ui/common/post/PostDetailFragment.kt
@@ -0,0 +1,93 @@
+package com.ssafy.intagral.ui.common.post
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.ssafy.intagral.databinding.ViewPostDetailBinding
+import com.ssafy.intagral.viewmodel.PostDetailViewModel
+
+class PostDetailFragment: Fragment() {
+
+    private var paramPostId: Int? = null
+
+    private lateinit var binding: ViewPostDetailBinding
+    private val postDetailViewModel: PostDetailViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            paramPostId = it.getInt(PARAM_NAME)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ViewPostDetailBinding.inflate(inflater, container, false)
+        
+        binding.include.profileSimpleFollowbtn.setOnClickListener {
+            postDetailViewModel.toggleWriterFollow(paramPostId!!)
+        }
+
+        binding.buttonLike.setOnClickListener{
+            postDetailViewModel.togglePostLike(paramPostId!!)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        postDetailViewModel.getPostDetail().observe(
+            viewLifecycleOwner
+        ){
+            it?.let {
+                Glide.with(requireContext())
+                    .load(it.writerImgPath)
+                    .into(binding.include.profileSimpleImg)
+                binding.include.profileSimpleNickname.text = it.writer
+
+                if(it.isFollow){
+                    binding.include.profileSimpleFollowbtn.text = "UNFOLLOW"
+                } else {
+                    binding.include.profileSimpleFollowbtn.text = "FOLLOW"
+                }
+
+                Glide.with(requireContext())
+                    .load(it.imgPath)
+                    .into(binding.postImage)
+                binding.postLikeCount.text = "${it.likeCount}"
+                if(it.tags.size > 0){
+                    binding.postContent.text = it.tags.reduce { acc, s -> "$acc #$s" }
+                }
+                binding.buttonLike.isChecked = it.isLike
+            }
+        }
+
+        postDetailViewModel.fetchPostDetail(paramPostId!!)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        postDetailViewModel.getPostDetail().value = null
+    }
+
+    companion object {
+        private const val PARAM_NAME = "paramPostId"
+        @JvmStatic
+        fun newInstance(postId: Int) =
+            PostDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(PARAM_NAME, postId)
+                }
+            }
+    }
+}
