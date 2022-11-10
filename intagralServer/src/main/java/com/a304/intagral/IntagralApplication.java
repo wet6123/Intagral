4fd880f44a483cfac3e3eda1ccfe2cc8b592cb5e
강봉민 <kbm4250@naver.com>
2022-11-10 09:51:25 +0900

    [SERVER] merge | SERVER/timezone into developServer/timezone

--- intagralServer/src/main/java/com/a304/intagral/IntagralApplication.java
+++ intagralServer/src/main/java/com/a304/intagral/IntagralApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class IntagralApplication {
@@ -15,4 +18,9 @@ public class IntagralApplication {
 		SpringApplication.run(IntagralApplication.class, args);
 	}
 
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+	}
 }
