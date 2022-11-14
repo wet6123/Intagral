bafa915b0b9330ea5086092d9ead03ad6ba8c4d6
강봉민 <kbm4250@naver.com>
2022-11-14 14:05:05 +0900

    [SERVER] modify | Add sorting in search result

--- intagralServer/src/main/java/com/a304/intagral/db/repository/HashtagRepository.java
+++ intagralServer/src/main/java/com/a304/intagral/db/repository/HashtagRepository.java
@@ -2,6 +2,7 @@ package com.a304.intagral.db.repository;
 
 import com.a304.intagral.db.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
     Optional<Hashtag> findByContent(String content);
 
+    @Query(value = "SELECT * FROM hashtag WHERE content LIKE CONCAT('%', :target, '%') ORDER BY CASE WHEN content = :target THEN 0 WHEN content LIKE CONCAT(:target, '%') THEN 1 WHEN content LIKE CONCAT('%', :target, '%') THEN 2 WHEN content `LIKE CONCAT('%', :target) THEN 3 ELSE 4 END", nativeQuery = true)
     List<Hashtag> findAllByContentContaining(String target);
 
     List<Hashtag> findTop5ByOrderBySearchCntDesc();
--- intagralServer/src/main/java/com/a304/intagral/db/repository/UserRepository.java
+++ intagralServer/src/main/java/com/a304/intagral/db/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.a304.intagral.db.repository;
 
 import com.a304.intagral.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,7 +19,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    List<User> findAllByNicknameContaining(String target);
+    @Query(value = "SELECT * FROM user WHERE nickname LIKE CONCAT('%', :target, '%') ORDER BY CASE WHEN nickname = :target THEN 0 WHEN nickname LIKE CONCAT(:target, '%') THEN 1 WHEN nickname LIKE CONCAT('%', :target, '%') THEN 2 WHEN nickname LIKE CONCAT('%', :target) THEN 3 ELSE 4 END", nativeQuery = true)
+    List<User> findAllByNicknameContaining(@Param(value = "target") String target);
 
     List<User> findByIdIn(List<Long> userIdList);
 
