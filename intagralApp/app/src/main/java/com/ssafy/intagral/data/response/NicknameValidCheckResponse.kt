d232300e9b1d8eec5abeb6808a530e3d592fc29b
한유연 <hyy0416@naver.com>
2022-11-19 23:56:22 +0900

    [APP] chore | switch case integer

--- intagralApp/app/src/main/java/com/ssafy/intagral/data/response/NicknameValidCheckResponse.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/data/response/NicknameValidCheckResponse.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class NicknameValidCheckResponse (
-    // 0: 괜찮음, 1: 닉네임 중복, 2: 너무 김,
+    // 0: 괜찮음, 2: 닉네임 중복, 1: 너무 김,
     @Expose
     @SerializedName(value = "isAvailable")
     val isAvailable: Int
