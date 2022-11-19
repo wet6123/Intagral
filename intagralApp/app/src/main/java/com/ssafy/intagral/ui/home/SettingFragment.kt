ef5bdf374df8f1b16fe2d481f19b380518c75df9
Taeyoon Jeong <tanficial9574@gmail.com>
2022-11-19 20:52:58 +0900

    [APP] feat | setting question feature

--- intagralApp/app/src/main/java/com/ssafy/intagral/ui/home/SettingFragment.kt
+++ intagralApp/app/src/main/java/com/ssafy/intagral/ui/home/SettingFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ssafy.intagral.IntagralApplication
 import com.ssafy.intagral.MainMenuActivity
 import com.ssafy.intagral.R
 import com.ssafy.intagral.databinding.FragmentSettingBinding
@@ -29,12 +30,13 @@ class SettingFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
-        binding.termsOfServiceButton.setOnClickListener(LogoutClickListener())
-        binding.logoutButton.setOnClickListener(LogoutClickListener())
+        binding.termsOfServiceButton.setOnClickListener(SettingClickListener())
+        binding.logoutButton.setOnClickListener(SettingClickListener())
+        binding.questionButton.setOnClickListener(SettingClickListener())
         return binding.root
     }
 
-    inner class LogoutClickListener : View.OnClickListener{
+    inner class SettingClickListener : View.OnClickListener{
         override fun onClick(p0: View?) {
             when(p0?.id){
                 R.id.terms_of_service_button -> {
@@ -43,6 +45,15 @@ class SettingFragment : Fragment() {
                         Uri.parse("https://k7a304.p.ssafy.io/intagral_policy.html")
                     )
                     startActivity(browserIntent)
+                }
+                R.id.question_button -> {
+                    val address = arrayOf("intagralofficial@gmail.com")
+                    val email = Intent(Intent.ACTION_SEND)
+                    email.type = "plain/text"
+                    email.putExtra(Intent.EXTRA_EMAIL, address)
+                    email.putExtra(Intent.EXTRA_SUBJECT, "${IntagralApplication.prefs.nickname} 님의 문의")
+                    email.putExtra(Intent.EXTRA_TEXT, "문의 내용 : ")
+                    startActivity(email)
                 }
                 R.id.logout_button -> {
                     val activity = requireActivity() as MainMenuActivity
